1716766f07e1076b1b807fd89f1dc7023b9a0665
ajcompany14151-art <ajcompany14151@gmail.com>
2025-10-07 06:34:20 +0530

    Update ChatInput.tsx

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -4,13 +4,7 @@ import { SendIcon } from './icons/SendIcon';
 import { PaperclipIcon } from './icons/PaperclipIcon';
 import { MicIcon } from './icons/MicIcon';
 import { SparklesIcon } from './icons/SparklesIcon';
-
-// Simple ShareIcon component to replace the missing import
-const ShareIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m9.032 4.026a9.001 9.001 0 01-7.432 0m9.032-4.026A9.001 9.001 0 0112 3c-4.474 0-8.268 3.12-9.032 7.326m0 0A9.001 9.001 0 0012 21c4.474 0 8.268-3.12 9.032-7.326" />
-  </svg>
-);
+import { ShareIcon } from './icons/CustomIcons';
 
 interface ChatInputProps {
   value: string;

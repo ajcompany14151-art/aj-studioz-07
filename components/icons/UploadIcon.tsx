db58063a85794ddcbc9fe94d5d44e9ff93a6a9b6
ajcompany14151-art <ajcompany14151@gmail.com>
2025-10-09 19:48:45 +0530

    Create UploadIcon.tsx

---
+++ components/icons/UploadIcon.tsx
@@ -0,0 +1,6 @@
+// components/icons/UploadIcon.tsx (New - for upload)
+import React from 'react';
+
+export const UploadIcon: React.FC<{ className?: string }> = ({ className = 'h-4 w-4' }) => (
+  <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12a2

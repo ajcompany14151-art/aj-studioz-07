dda1c9294b88c6d128700ae4734fcb422de6c480
ajcompany14151-art <ajcompany14151@gmail.com>
2025-10-09 19:42:15 +0530

    Create ShareIcon.tsx

---
+++ components/icons/ShareIcon.tsx
@@ -0,0 +1,8 @@
+// components/icons/ShareIcon.tsx (New file to fix import error)
+import React from 'react';
+
+export const ShareIcon: React.FC<{ className?: string }> = ({ className = 'h-4 w-4' }) => (
+  <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m9.032 4.026a9.001 9.001 0 01-7.432 0m9.032-4.026A9.001 9.001 0 0112 3c-4.474 0-8.268 3.12-9.032 7.326m0 0A9.001 9.001 0 0012 21c4.474 0 8.268-3.12 9.032-7.326" />
+  </svg>
+);
